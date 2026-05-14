d401e146e326e283f5abfa27714a2709af77a622
Zilver-Net <bgm-perfect.c@hotmail.com>
2026-05-14 16:30:09 +0700

    Set PACKETVER to 20250716 in custom defines

--- Server/src/custom/defines_pre.hpp
+++ Server/src/custom/defines_pre.hpp
@@ -8,7 +8,7 @@
  * rAthena configuration file (http://rathena.org)
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
-#define PACKETVER 20211103
+#define PACKETVER 20250716
 
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
